652b5efd1f216097390128ef809f56bb7593905a
jaythaceo <jaythaceo@gmail.com>
2017-05-17 09:22:32 -0400

    [chore] BFS function added

--- graphs/graph_algorithms.py
+++ graphs/graph_algorithms.py
@@ -2,3 +2,18 @@ from collections import deque
 from copy import deepcopy
 from union_find.unionfind import UnionFind
 import heapq
+
+def BFS(gr, s):
+	"""Breath first search
+	Returns a list of nodes that are "findable" from s"""
+	if not gr.has_node(s):
+		raise Exception("Node %s not in graph" % s)
+	nodes_explored = set([s])
+	q = deque([s])
+	while len(q) !=0:
+		node = q.popleft()
+		for each in gr.neighbors(node):
+			if each not in nodes_explored:
+				nodes_explored.add(each)
+				q.append(each)
+	return nodes_explored
