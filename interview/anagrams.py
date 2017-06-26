1761a7b1c24c7e33a34a8bb454891354256d5e8b
jaythaceo <jaythaceo@gmail.com>
2017-06-26 13:56:42 -0400

    [chore] Added C++ folder and file

---
+++ interview/anagrams.py
@@ -0,0 +1,8 @@
+# Anagram method
+
+numbers = [1,3,3]
+
+product = 1
+for i in numbers:
+	product *= i
+print product
--- trees/runway.py
+++ trees/runway.py
@@ -4,8 +4,6 @@ time = t
 reservations = r[]
 Add t to the set r if no landings are in k minutes
 """
+from collections import deque
 
 ar = [2,45,21,34]
-
-for i in ar:
-	print i
